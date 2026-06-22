c54569f53963a8fc225018044e8960adf53e3df0
divakar singh purva <divakarpurva04@gmail.com>
2026-06-23 05:07:46 +0530

    feat: Add index for product updates and update schema definition

---
+++ drizzle/0002_dry_sentry.sql
@@ -0,0 +1,1 @@
+CREATE INDEX "idx_products_updated_id" ON "products" USING btree ("updated_at","id");
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,13 @@
-import { integer, pgEnum, pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+    index,
+    integer,
+    pgEnum,
+    pgTable,
+    serial,
+    timestamp,
+    varchar,
+} from "drizzle-orm/pg-core";
+import { table } from "node:console";
 
 export const categoryEnum = pgEnum("category", [
     "Electronics",
@@ -12,11 +21,23 @@ export const categoryEnum = pgEnum("category", [
     "Other",
 ]);
 
-export const productTable = pgTable("products", {
-    id: serial("id").primaryKey(),
-    name: varchar("name").notNull(),
-    category: categoryEnum("category").default("Other").notNull(),
-    price: integer("price").notNull(),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").$onUpdate(() => new Date()).notNull()
-});
+export const productTable = pgTable(
+    "products",
+    {
+        id: serial("id").primaryKey(),
+        name: varchar("name").notNull(),
+        category: categoryEnum("category").default("Other").notNull(),
+        price: integer("price").notNull(),
+        createdAt: timestamp("created_at").defaultNow().notNull(),
+        updatedAt: timestamp("updated_at")
+            .$onUpdate(() => new Date())
+            .notNull(),
+    },
+    (table) => ({
+        updatedIdIdx: index("idx_products_updated_id").on(
+            table.updatedAt,
+            table.id,
+        ),
+    }),
+);
+
