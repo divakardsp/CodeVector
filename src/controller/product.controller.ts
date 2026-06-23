3522d490d6a314e2f15b939f4955d13601a4d4c6
divakar singh purva <divakarpurva04@gmail.com>
2026-06-23 07:52:17 +0530

    feat: Enhance product update functionality and add cursor pagination for product retrieval

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,46 +1,109 @@
-import type { Request, Response } from "express"
+import type { Request, Response } from "express";
 import { db } from "../db/index.js";
 import { productTable } from "../db/schema.js";
-import { eq } from "drizzle-orm";
+import { and, desc, eq, lt, or } from "drizzle-orm";
 
 export const updateProductPrice = async (req: Request, res: Response) => {
-    const {id} = req.params;
-    const {price} = req.body;
+    try {
+        const { id } = req.params;
+        const { price } = req.body;
 
-    if(!id){
+        if (!id) {
+            return res.json({
+                statusCode: 401,
+                message: "Id is missing",
+                success: false,
+            });
+        }
+
+        const product = await db
+            .select()
+            .from(productTable)
+            .where(eq(productTable.id, Number(id)));
+
+        if (!product) {
+            return res.json({
+                statusCode: 404,
+                message: "Product with this id does not exist",
+                success: false,
+            });
+        }
+
+        const updatedProduct = await db
+            .update(productTable)
+            .set({
+                price,
+            })
+            .where(eq(productTable.id, Number(id)))
+            .returning();
+
         return res.json({
-            statusCode: 401,
-            message: "Id is missing",
-            success: false,
-        })
+            statusCode: 201,
+            message: "Product Updated",
+            data: updatedProduct,
+            success: true,
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            throw new Error(error.message);
+        }
+        console.log(error);
     }
+};
 
-    const product = await db
-        .select()
-        .from(productTable)
-        .where(eq(productTable.id, Number(id)))
+export const getProductsCursorPagination = async (
+    req: Request,
+    res: Response,
+) => {
+    try {
 
-    if(!product){
+        const cursor = req.body?.cursor || {};
+        const { limit = 20, category = null } = req.query;
+
+        const keysLengthOfCursor = Object.keys(cursor).length;
+
+        const conditions = [];
+
+        if (category) {
+            conditions.push(eq(productTable.category, category));
+        }
+
+        if (keysLengthOfCursor !== 0) {
+            const cursorCreatedAtDate = new Date(cursor.createdAt);
+            conditions.push(
+                or(
+                    lt(productTable.createdAt, cursorCreatedAtDate),
+                    and(
+                        eq(productTable.createdAt, cursorCreatedAtDate),
+                        lt(productTable.id, cursor.id),
+                    ),
+                ),
+            );
+        }
+
+        const products = await db
+            .select()
+            .from(productTable)
+            .where(conditions.length ? and(...conditions) : undefined)
+            .orderBy(desc(productTable.createdAt), desc(productTable.id))
+            .limit(Number(limit));
+
+        const lastProduct = Number(limit) - 1;
+        const cursorCreatedAt = products[lastProduct].createdAt;
+        const cursorId = products[lastProduct].id;
+        const nextCursor = { createdAt: cursorCreatedAt, id: cursorId };
+
         return res.json({
-            statusCode: 404,
-            message: "Product with this id does not exist",
-            success: false,
-        })
+            statusCode: 201,
+            data: { products },
+            success: true,
+            cursor: nextCursor,
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            throw new Error(error.message);
+        }
+
+        console.log(error);
     }
-
-    const updatedProduct = await db    
-        .update(productTable)
-        .set({
-            price
-        })
-        .where(eq(productTable.id, Number(id)))
-        .returning();
-
-    return res.json({
-        statusCode: 201,
-        message: "Product Updated",
-        data: updatedProduct,
-        success: true,
-    })
-}
-
+};
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { updateProductPrice } from "../controller/product.controller.js";
+import { getProductsCursorPagination, updateProductPrice } from "../controller/product.controller.js";
 
 
 const router = Router();
 
 router.post("/update-product/:id", updateProductPrice);
+router.get("/get-products", getProductsCursorPagination)
 
 
 export default router;
